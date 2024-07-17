a169b81c1de87161e4d7f52626959d955642297b
solesen1992 <saramundal@live.dk>
2024-07-17 16:15:23 +0200

    Copy in the model layer

--- src/model/Copy.java
+++ src/model/Copy.java
@@ -1,5 +1,62 @@
 package model;
 
-public class Copy {
+import java.util.Date;
 
+/**
+ * This class contains information about a copy: SerialNumber, purchaseDate and purchasePrice.
+ * Furthermore, it has a reference to the LP-class which it is a copy of.
+ * */
+
+public class Copy {
+	private String serialNumber;
+	private Date purchaseDate;
+	private double purchasePrice;
+	private LP lp;
+	
+	/**
+	 * Constructor
+	 * */
+	public Copy(String serialNumber, Date purchaseDate, double purchasePrice, LP lp) {
+		super();
+		this.serialNumber = serialNumber;
+		this.purchaseDate = purchaseDate;
+		this.purchasePrice = purchasePrice;
+		this.lp = lp;
+	}
+
+	/**
+	 * Get and set methods
+	 * */
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	public LP getLp() {
+		return lp;
+	}
+
+	public void setLp(LP lp) {
+		this.lp = lp;
+	}
+	
 }
