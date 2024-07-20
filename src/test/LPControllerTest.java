4ae7caa680593d5c1b245b4bf46d738f2e28ecd4
solesen1992 <saramundal@live.dk>
2024-07-20 11:24:13 +0200

    Update LpControllerTest

--- src/test/LPControllerTest.java
+++ src/test/LPControllerTest.java
@@ -1,5 +1,34 @@
 package test;
 
-public class LPControllerTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LPControllerTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 
 }
